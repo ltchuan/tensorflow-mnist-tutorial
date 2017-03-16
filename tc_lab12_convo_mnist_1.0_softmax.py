98774bffa79c19c86b54b29e6bc1d4164007aadd
ltchuan <ltchuan@gmail.com>
2017-03-16 20:37:37 +1300

    Finished the codelab

---
+++ tc_lab12_convo_mnist_1.0_softmax.py
@@ -0,0 +1,160 @@
+# encoding: UTF-8
+# Copyright 2016 Google.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import math as math
+import tensorflow as tf
+import tensorflowvisu
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+tf.set_random_seed(0)
+
+# neural network with 1 layer of 10 softmax neurons
+#
+# · · · · · · · · · ·       (input data, flattened pixels)       X [batch, 784]        # 784 = 28 * 28
+# \x/x\x/x\x/x\x/x\x/    -- fully connected layer (softmax)      W [784, 10]     b[10]
+#   · · · · · · · ·                                              Y [batch, 10]
+
+# The model is:
+#
+# Y = softmax( X * W + b)
+#              X: matrix for 100 grayscale images of 28x28 pixels, flattened (there are 100 images in a mini-batch)
+#              W: weight matrix with 784 lines and 10 columns
+#              b: bias vector with 10 dimensions
+#              +: add with broadcasting: adds the vector to each line of the matrix (numpy)
+#              softmax(matrix) applies softmax on each line
+#              softmax(line) applies an exp to each value then divides by the norm of the resulting line
+#              Y: output matrix with 100 lines and 10 columns
+
+# Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+labels)
+mnist = read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
+
+# Learning rate parameters
+lr_min = 0.0001
+lr_max = 0.003
+
+pkeep_input = 0.75
+
+learning_rate = tf.placeholder(tf.float32)
+pkeep = tf.placeholder(tf.float32)
+
+# input X: 28x28 grayscale images, the first dimension (None) will index the images in the mini-batch
+X = tf.placeholder(tf.float32, [None, 28, 28, 1])
+# correct answers will go here
+Y_ = tf.placeholder(tf.float32, [None, 10])
+# weights W[784, (Layer 1)]   784=28*28
+# Layer 1: 4 channels
+W1 = tf.Variable(tf.truncated_normal([5, 5, 1, 4], stddev=0.1))
+# biases b[4]
+b1 = tf.Variable(tf.ones([4])/10)
+
+# Layer 2: 8 channels
+W2 = tf.Variable(tf.truncated_normal([4, 4, 4, 8], stddev=0.1))
+b2 = tf.Variable(tf.ones([8])/10)
+
+# Layer 3: 12 channels
+W3 = tf.Variable(tf.truncated_normal([4, 4, 8, 12], stddev=0.1))
+b3 = tf.Variable(tf.ones([12])/10)
+
+# Layer 4: normal fully connected layer, 200 neurons
+W4 = tf.Variable(tf.truncated_normal([7*7*12, 200], stddev=0.1))
+b4 = tf.Variable(tf.ones([200])/10)
+
+# Output layer connection
+W_o = tf.Variable(tf.truncated_normal([200, 10], stddev=0.1))
+b_o = tf.Variable(tf.ones([10])/10)
+
+
+# The model
+# Layers + dropout
+Y1cnv = tf.nn.conv2d(X, W1, strides=[1, 1, 1, 1], padding='SAME')
+Y1 = tf.nn.relu(Y1cnv + b1)
+Y2cnv = tf.nn.conv2d(Y1, W2, strides=[1, 2, 2, 1], padding='SAME')
+Y2 = tf.nn.relu(Y2cnv + b2)
+Y3cnv = tf.nn.conv2d(Y2, W3, strides=[1, 2, 2, 1], padding='SAME')
+Y3 = tf.nn.relu(Y3cnv + b3)
+Y3_flat = tf.reshape(Y3, [-1, 7*7*12])
+Y4 = tf.nn.relu(tf.matmul(Y3_flat, W4) + b4)
+
+# Output
+Y_o_logits = tf.matmul(Y4, W_o) + b_o
+Y_o = tf.nn.softmax(Y_o_logits)
+
+# loss function: cross-entropy = - sum( Y_i * log(Yi) )
+#                           Y_o: the computed output vector
+#                           Y_: the desired output vector
+
+# cross-entropy
+# log takes the log of each element, * multiplies the tensors element by element
+# reduce_mean will add all the components in the tensor
+# so here we end up with the total cross-entropy for all images in the batch
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Y_o_logits, labels=Y_) # normalized later,
+cross_entropy = tf.reduce_mean(cross_entropy)*100
+
+# accuracy of the trained model, between 0 (worst) and 1 (best)
+correct_prediction = tf.equal(tf.argmax(Y_o, 1), tf.argmax(Y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# training, learning rate adaptive
+train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+
+# matplotlib visualisation
+allweights = tf.concat([tf.reshape(W_o, [-1]), tf.reshape(W1, [-1]), tf.reshape(W2, [-1]), tf.reshape(W3, [-1]), tf.reshape(W4, [-1])], 0)
+allbiases = tf.concat([tf.reshape(b_o, [-1]), tf.reshape(b1, [-1]), tf.reshape(b2, [-1]), tf.reshape(b3, [-1]), tf.reshape(b4, [-1])], 0)
+I = tensorflowvisu.tf_format_mnist_images(X, Y_o, Y_)  # assembles 10x10 images by default
+It = tensorflowvisu.tf_format_mnist_images(X, Y_o, Y_, 1000, lines=25)  # 1000 images on 25 lines
+datavis = tensorflowvisu.MnistDataVis()
+
+# init
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+
+# You can call this function in a loop to train the model, 100 images at a time
+def training_step(i, update_test_data, update_train_data):
+
+    # adaptive learning rate
+    learning_rate_input = lr_min + (lr_max - lr_min)*math.exp(-i/2000)
+
+    # training on batches of 100 images with 100 labels
+    batch_X, batch_Y = mnist.train.next_batch(100)
+
+    # compute training values for visualisation
+    if update_train_data:
+        a, c, im, w, b = sess.run([accuracy, cross_entropy, I, allweights, allbiases], feed_dict={X: batch_X, Y_: batch_Y, pkeep: 1})
+        datavis.append_training_curves_data(i, a, c)
+        datavis.append_data_histograms(i, w, b)
+        datavis.update_image1(im)
+        print(str(i) + ": accuracy:" + str(a) + " loss: " + str(c) + " (lr:" + str(learning_rate_input) + ")")
+
+    # compute test values for visualisation
+    if update_test_data:
+        a, c, im = sess.run([accuracy, cross_entropy, It], feed_dict={X: mnist.test.images, Y_: mnist.test.labels, pkeep: 1})
+        datavis.append_test_curves_data(i, a, c)
+        datavis.update_image2(im)
+        print(str(i) + ": ********* epoch " + str(i*100//mnist.train.images.shape[0]+1) + " ********* test accuracy:" + str(a) + " test loss: " + str(c))
+
+    # the backpropagation training step
+    sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y, learning_rate: learning_rate_input, pkeep: pkeep_input})
+
+
+datavis.animate(training_step, iterations=10000+1, train_data_update_freq=20, test_data_update_freq=100, more_tests_at_start=True)
+
+# to save the animation as a movie, add save_movie=True as an argument to datavis.animate
+# to disable the visualisation use the following line instead of the datavis.animate line
+# for i in range(2000+1): training_step(i, i % 50 == 0, i % 10 == 0)
+
+print("max test accuracy: " + str(datavis.get_max_test_accuracy()))
+
+# final max test accuracy = 0.9268 (10K iterations). Accuracy should peak above 0.92 in the first 2000 iterations.
